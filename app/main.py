c8c0c0ccd4c87539270cffb2b7122144590926aa
Jorge Enrique <jorge@gmail.com>
2025-07-26 20:35:10 -0500

    mini cambio

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-# main.py
 from fastapi import FastAPI
+from app.routers import users  # Importa el router de usuarios
 
 app = FastAPI()
 
+app.include_router(users.router)  # Añade el router de usuarios
+
 @app.get("/")
-def read_root():
-    return {"message": "¡Funciona CodeForceCoach!"}
+async def root():
+    return {"message": "¡CodeForceCoach funcionando!"}
